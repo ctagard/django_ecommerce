c0be542c10560774f7abfb96e984718817316e12
ctagard <ctagard19@gmail.com>
2023-03-31 20:14:05 -0400

    [backend/frontend] added login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer'
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -15,7 +17,10 @@ function App() {
        <Container>
            <Routes>
                <Route  path="/" element={<HomeScreen />}/>
+               <Route  path={"/login"} element={<LoginScreen />} />
                <Route  path="/product/:id" element={<ProductScreen />} />
+               <Route  path={"/cart/:id?"} element={<CartScreen />} />
+
            </Routes>
        </Container>
       </main>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import {Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import React, {useState, useEffect} from 'react'
+import {Link, useParams, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Button, Card, Form} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../actions/productActions'
+
 
 function ProductScreen() {
     const params = useParams();
-    const product = products.find((p) => p._id === params.id)
+    const dispatch = useDispatch()
+    let navigate = useNavigate()
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading, error, product} = productDetails
+
+    const [qty, setQty] = useState(1)
+
+    useEffect(() => {
+            dispatch(listProductDetails(params.id))
+    },[dispatch, params])
+
+    const addToCartHandler = () => {
+        navigate(`/cart/${params.id}?qty=${qty}`)
+    }
+
     return (
         <div>
             <Link to={'/'} className={'btn btn-light my-3'}>Go Back</Link>
-            <Row>
+            {loading ?
+                <Loader />
+                : error
+                ? <Message variant={'danger'}>{error}</Message>
+                :
+                (
+                <Row>
                 <Col md={6}>
                    <Image src={product.image} alt={product.name} fluid />
                 </Col>
@@ -53,8 +77,35 @@ function ProductScreen() {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col xs={'auto'} className={'my-1'}>
+                                            <Form.Control
+                                                as={'select'}
+                                                value={qty}
+                                                onChange={(e) => setQty(e.target.value)}
+                                            >
+                                                {
+                                                    [...Array(product.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>
+                                                            {x + 1}
+                                                        </option>
+                                                    ))
+                                                }
+
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
                             <ListGroup.Item>
-                               <Button className={'btn-block'} type={'button'} disabled={product.countInStock === 0}>
+                               <Button
+                                   onClick={addToCartHandler}
+                                   className={'btn-block'}
+                                   type={'button'}
+                                   disabled={product.countInStock === 0}>
                                    Add To Cart
                                </Button>
                             </ListGroup.Item>
@@ -62,6 +113,10 @@ function ProductScreen() {
                     </Card>
                 </Col>
             </Row>
+        )
+
+            }
+
         </div>
     )
 }
