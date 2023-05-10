4c4c5149257f2b0014be07cad2b609a01ca0792f
ctagard <ctagard19@gmail.com>
2023-05-10 16:18:20 -0400

    attempting to fix heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.environ.get("HEROKU") == "true":
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # Application definition
